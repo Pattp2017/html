a78731a559063174246621de43097ea363649584
Pattp2017 <pafptp@gmail.com>
2025-08-28 20:55:56 -0300

    Delete assets directory

--- assets/js/site.js
+++
@@ -1,6 +0,0 @@
-
-(function(){
-  const links = document.querySelectorAll('.toc a');
-  const here = location.pathname.split('/').pop() || 'index.html';
-  for(const a of links){ if(a.getAttribute('href') === here){ a.classList.add('active'); } }
-})();
