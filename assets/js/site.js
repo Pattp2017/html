0b1be3d3952e6477911226e89c6681d18dcfbe2f
Pattp2017 <pafptp@gmail.com>
2025-08-28 21:54:55 -0300

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,3 +4,17 @@
   const here = location.pathname.split('/').pop() || 'index.html';
   for(const a of links){ if(a.getAttribute('href') === here){ a.classList.add('active'); } }
 })();
+<script>
+  (function(){
+    function hideMenus(){
+      document.querySelectorAll(
+        '.sidebar,.topnav,.bottomnav,.sidebar .menu,.toc,.kit,#menu-fab,.backdrop'
+      ).forEach(el => el && el.remove());
+    }
+    // remove imediatamente e novamente após scripts carregarem
+    hideMenus();
+    setTimeout(hideMenus, 0);
+    setTimeout(hideMenus, 200);
+  })();
+</script>
+
